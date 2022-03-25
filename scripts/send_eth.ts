53c054402a3dcca97c9dd6ea50515a4cb879b83c
MatthiasWanner <matthias.wanner@gmail.com>
2022-03-25 12:58:31 +0100

    :sparkles: (feat) Send wallet to wallet eth using ethers

---
+++ scripts/send_eth.ts
@@ -0,0 +1,33 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers } from "hardhat";
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const signers = await ethers.getSigners();
+  const receiverAddress = await signers[signers.length - 1].getAddress();
+  const sender = signers[signers.length - 3];
+  const transaction = await sender.sendTransaction({
+    to: receiverAddress,
+    value: ethers.utils.parseEther("5000"),
+  });
+
+  console.log("Your transaction: ", transaction);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
